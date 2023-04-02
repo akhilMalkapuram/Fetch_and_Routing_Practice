811b6c1d512c40c2e35e66aae42130f2f95d8669
akhilMalkapuram <malkapuramakhil2196@gmail.com>
2023-04-02 10:17:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-import RandomNumber from "./Components/UserProfile";
-const App=()=> <RandomNumber/>
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+import Header from './Components/Header'
+import About from './Components/About'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+import NotFound from './Components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+  <div className="app-container">
+    <div className="responsive-container">
+      <Header />
+      <div className="app-body">
+      
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About/>} />
+          <Route exact path="/contact" element={<Contact/>} />
+          <Route element={<NotFound/>} />
+        </Routes>
+       
+      </div>
+    </div>
+  </div>
+  </BrowserRouter>
+)
+
 export default App
